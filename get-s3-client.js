85179d1a3af0b4a49aed0bc0170a91fbd869a752
Hasan <44682484+hsnd90@users.noreply.github.com>
2023-06-05 00:11:22 +0300

    some fixes.

--- get-s3-client.js
+++ get-s3-client.js
@@ -5,11 +5,11 @@ let production = process.env.NODE_ENV === 'prod';
 function getClient() {
   if (production) {
     client = new S3Client({
-      region: process.env.REGION,
+      region: process.env.REGION ?? 'eu-north-1',
     });
   } else {
     client = new S3Client({
-      region: process.env.REGION,
+      region: process.env.REGION ?? 'eu-north-1',
       credentials: {
         secretAccessKey: process.env.ACCESS_SECRET,
         accessKeyId: process.env.ACCESS_KEY,
