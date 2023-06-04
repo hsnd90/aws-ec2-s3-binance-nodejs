6ec8fe41b5882d43f081c8cfb4dd7d39ff868117
Hasan <44682484+hsnd90@users.noreply.github.com>
2023-06-05 00:32:46 +0300

    some fixes.

--- get-s3-client.js
+++ get-s3-client.js
@@ -4,15 +4,12 @@ let production = process.env.NODE_ENV === 'prod';
 
 console.log('AWS_SECRET_ACCESS_KEY', process.env.AWS_SECRET_ACCESS_KEY);
 console.log('AWS_ACCESS_KEY_ID', process.env.AWS_ACCESS_KEY_ID);
+console.log('NODE_ENV', process.env.NODE_ENV);
 
 function getClient() {
   if (production) {
     client = new S3Client({
       region: process.env.REGION ?? 'eu-north-1',
-      // credentials: {
-      //   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-      //   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      // },
     });
   } else {
     client = new S3Client({
--- run.sh
+++ run.sh
@@ -1,13 +1,8 @@
 cd /home/ec2-user || exit
 sudo dnf upgrade -y
 yes y | sudo dnf install nodejs git
-# sudo dnf install --assumeyes python3-pip libffi-devel openssl-devel bzip2-devel git
-git clone https://github.com/hsnd90/binance-nodejs.git
-sudo chown -R ec2-user:ec2-user /home/ec2-user/binance-nodejs
-# sudo chmod 2775 /home/ec2-user/binance_5 && find /home/ec2-user/binance_5 -type d -exec sudo chmod 2775 {} \;
-cd binance-nodejs || exit
+git clonehttps://github.com/hsnd90/aws-ec2-s3-binance-nodejs.git
+sudo chown -R ec2-user:ec2-user /home/ec2-user/aws-ec2-s3-binance-nodejs
+cd aws-ec2-s3-binance-nodejs || exit
 npm install
-python3 -m venv venv
-source venv/bin/activate
-pip3 install -r requirements.txt
-python3 main.py
+NODE_ENV=prod node index.js
