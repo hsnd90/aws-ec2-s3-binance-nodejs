d5724413ecdfdaadb4450fbf47c06651450ae416
Hasan <44682484+hsnd90@users.noreply.github.com>
2023-06-05 00:25:51 +0300

    some fixes.

--- get-s3-client.js
+++ get-s3-client.js
@@ -2,6 +2,9 @@ const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
 
 let production = process.env.NODE_ENV === 'prod';
 
+console.log('AWS_SECRET_ACCESS_KEY', process.env.AWS_SECRET_ACCESS_KEY);
+console.log('AWS_ACCESS_KEY_ID', process.env.AWS_ACCESS_KEY_ID);
+
 function getClient() {
   if (production) {
     client = new S3Client({
